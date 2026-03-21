f21cb3bec698f8254e786748e3134494d91adee7
Bhaskar Mudaliyar <mudaliyarbhaskar29@gmail.com>
2026-03-21 13:47:30 +0530

    adding endpoint for helloworld pathvariable

--- Bhaskar_code/Restfull-Web_services/src/main/java/com/practise/rest/webservice/controller/HelloWorldController.java
+++ Bhaskar_code/Restfull-Web_services/src/main/java/com/practise/rest/webservice/controller/HelloWorldController.java
@@ -1,7 +1,10 @@
 package com.practise.rest.webservice.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.practise.rest.webservice.model.HelloWorldBean;
 
 @RestController
 public class HelloWorldController {
@@ -9,5 +12,15 @@ public class HelloWorldController {
 	@GetMapping(path = "/helloWorld")
 	public String helloWorld() {
 		return "Hello World";
+	}
+
+	@GetMapping(path = "/helloWorldBean")
+	public HelloWorldBean helloWorldBean() {
+		return new HelloWorldBean("Hello World");
+	}
+
+	@GetMapping(path = "/helloWorld/pathvariable/{name}")
+	public HelloWorldBean helloWorldPathvariable(@PathVariable String name) {
+		return new HelloWorldBean(String.format("Hello World , %s ", name));
 	}
 }
