ebf1e29389f8022babfa7e9b4f90c0e7f30221a9
Bhaskar Mudaliyar <mudaliyarbhaskar29@gmail.com>
2026-03-21 09:11:10 +0530

    adding endpoint for helloWorld

---
+++ Bhaskar_code/Restfull-Web_services/src/main/java/com/practise/rest/webservice/controller/HelloWorldController.java
@@ -0,0 +1,13 @@
+package com.practise.rest.webservice.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+	@GetMapping(path = "/helloWorld")
+	public String helloWorld() {
+		return "Hello World";
+	}
+}
