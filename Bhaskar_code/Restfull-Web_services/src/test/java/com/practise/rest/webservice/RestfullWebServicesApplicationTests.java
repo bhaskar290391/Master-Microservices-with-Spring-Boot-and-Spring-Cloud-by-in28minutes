2eb424f72a8f610b0253377d8289d4614b7250c0
Bhaskar Mudaliyar <mudaliyarbhaskar29@gmail.com>
2026-03-21 08:58:48 +0530

    adding skeleton for restfull web service

---
+++ Bhaskar_code/Restfull-Web_services/src/test/java/com/practise/rest/webservice/RestfullWebServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practise.rest.webservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfullWebServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
