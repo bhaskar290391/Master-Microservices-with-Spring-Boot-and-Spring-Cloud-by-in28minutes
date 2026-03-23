bb0f636c48b22dd05d75879d0254f868027ca95d
Bhaskar Mudaliyar <mudaliyarbhaskar29@gmail.com>
2026-03-23 17:26:38 +0530

    static filtering

---
+++ Bhaskar_code/Restfull-Web-services/src/main/java/com/practise/rest/webservice/controller/FilteringController.java
@@ -0,0 +1,24 @@
+package com.practise.rest.webservice.controller;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.practise.rest.webservice.entity.SomeBean;
+
+@RestController
+public class FilteringController {
+
+	@GetMapping("/filtering")
+	public SomeBean filtering() {
+		return new SomeBean("value1","value2","value3");
+	}
+	
+	@GetMapping("/filtering-list")
+	public List<SomeBean> filteringList() {
+		return Arrays.asList(new SomeBean("value1","value2","value3"),
+				new SomeBean("value4","value5","value6"));
+	}
+}
