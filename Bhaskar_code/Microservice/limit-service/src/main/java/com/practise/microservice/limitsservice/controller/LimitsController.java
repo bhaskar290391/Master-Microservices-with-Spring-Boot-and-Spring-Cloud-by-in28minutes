3091e68b894836dbc126516e7c88628375fdb2b9
Bhaskar Mudaliyar <mudaliyarbhaskar29@gmail.com>
2026-03-23 19:42:24 +0530

    fetching limits from configration file

--- Bhaskar_code/Microservice/limit-service/src/main/java/com/practise/microservice/limitsservice/controller/LimitsController.java
+++ Bhaskar_code/Microservice/limit-service/src/main/java/com/practise/microservice/limitsservice/controller/LimitsController.java
@@ -1,15 +1,21 @@
 package com.practise.microservice.limitsservice.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.practise.microservice.limitsservice.bean.Limits;
+import com.practise.microservice.limitsservice.config.LimitsConfiguration;
 
 @RestController
 public class LimitsController {
 
+	@Autowired
+	private LimitsConfiguration configuration;
+
 	@GetMapping("/limits")
 	public Limits retrieveLimits() {
-		return new Limits(1,1000);
+		// return new Limits(1,1000);
+		return new Limits(configuration.getMinimum(), configuration.getMaximum());
 	}
 }
--- README.md
+++ README.md
@@ -3,4 +3,24 @@ Master Microservices with Spring Boot and Spring Cloud by in28minutes
 
 https://github.com/in28minutes/spring-microservices-v3
 
-https://github.com/in28minutes/spring-microservices-v3
+# standarized Ports
+1. Limits Microservice
+Ports: 8080, 8081, etc.
+
+2. Spring Cloud Config Server
+Port: 8888
+
+3. Currency Exchange Microservice
+Ports: 8000, 8001, 8002, etc.
+
+4. Currency Conversion Microservice
+Ports: 8100, 8101, 8102, etc.
+
+5. Netflix Eureka Naming Server
+Port: 8761
+
+6. API Gateway
+Port: 8765
+
+7. Zipkin Distributed Tracing Server
+Port: 9411
