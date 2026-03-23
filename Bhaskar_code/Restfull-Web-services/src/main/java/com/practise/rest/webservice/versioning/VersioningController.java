71a15a2584d1a91a6ad50618a671ef2dd158b229
Bhaskar Mudaliyar <mudaliyarbhaskar29@gmail.com>
2026-03-23 10:37:30 +0530

    adding version using params ,header and mediatype

--- Bhaskar_code/Restfull-Web-services/src/main/java/com/practise/rest/webservice/versioning/VersioningController.java
+++ Bhaskar_code/Restfull-Web-services/src/main/java/com/practise/rest/webservice/versioning/VersioningController.java
@@ -8,11 +8,42 @@ public class VersioningController {
 
 	@GetMapping("/v1/person")
 	public PersonV1 versioningPerson1() {
-		return new PersonV1("Bhaskar Mudaliyar"); 
+		return new PersonV1("Bhaskar Mudaliyar");
 	}
-	
+
 	@GetMapping("/v2/person")
 	public PersonV2 versioningPerson2() {
-		return new PersonV2(new Name("Bhaskar"," Mudaliyar")); 
+		return new PersonV2(new Name("Bhaskar", " Mudaliyar"));
+	}
+
+	@GetMapping(path = "/person", params = "version=1")
+	public PersonV1 versioningPerson1UsingParam() {
+		return new PersonV1("Bhaskar Mudaliyar");
+	}
+
+	@GetMapping(path = "/person", params = "version=2")
+	public PersonV2 versioningPerson2UsingParam() {
+		return new PersonV2(new Name("Bhaskar", " Mudaliyar"));
+	}
+
+	@GetMapping(path = "/person", headers = "x-api-version=1")
+	public PersonV1 versioningPerson1UsingHeaders() {
+		return new PersonV1("Bhaskar Mudaliyar");
+	}
+
+	@GetMapping(path = "/person", headers = "x-api-version=2")
+	public PersonV2 versioningPerson2UsingHeaders() {
+		return new PersonV2(new Name("Bhaskar", " Mudaliyar"));
+	}
+	
+	
+	@GetMapping(path = "/persons", produces = "application/test1")
+	public PersonV1 versioningPerson1UsingHeaderAccept() {
+		return new PersonV1("Bhaskar Mudaliyar");
+	}
+
+	@GetMapping(path = "/persons", produces = "application/test2")
+	public PersonV2 versioningPerson2UsingHeaderAccept() {
+		return new PersonV2(new Name("Bhaskar", " Mudaliyar"));
 	}
 }
