f071d97d513db3838338f1403871b105b29de205
Bhaskar Mudaliyar <mudaliyarbhaskar29@gmail.com>
2026-03-24 19:32:37 +0530

    adding feign client to currency conversion and to use currency exchange

--- Bhaskar_code/Microservice/currency-conversion/src/main/java/com/practise/microservice/controller/CurrencyConversionController.java
+++ Bhaskar_code/Microservice/currency-conversion/src/main/java/com/practise/microservice/controller/CurrencyConversionController.java
@@ -2,27 +2,33 @@ package com.practise.microservice.controller;
 
 import java.math.BigDecimal;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.practise.microservice.entity.CurrencyConversion;
+import com.practise.microservice.feign.CurrencyExchangeFiegnClient;
 
 @RestController
 public class CurrencyConversionController {
 
-	@Autowired
-	private Environment environment;
+	private CurrencyExchangeFiegnClient client;
+	
+	public CurrencyConversionController(CurrencyExchangeFiegnClient client) {
+		super();
+		this.client = client;
+	}
 
 	@GetMapping("/currency-conversion/from/{from}/to/{to}/quantity/{quantity}")
 	public CurrencyConversion retrieveCurrencyConversionData(@PathVariable String from, @PathVariable String to,
 			@PathVariable BigDecimal quantity) {
-		CurrencyConversion currencyConversion = new CurrencyConversion(1000L, from, to, quantity, BigDecimal.ONE,
-				BigDecimal.TWO, "");
-		String property = environment.getProperty("local.server.port");
-		currencyConversion.setEnvironment(property);
+		CurrencyConversion currencyConversion = client.retrieveCurrencyData(from, to);
+		currencyConversion.setQuantity(quantity);
+		currencyConversion.setTotalCalculatedAmount(quantity.multiply(currencyConversion.getConversionMultiple()));
 		return currencyConversion;
 	}
+
+
+
+	
 }
---
+++ Bhaskar_code/Microservice/currency-conversion/src/main/java/com/practise/microservice/feign/CurrencyExchangeFiegnClient.java
@@ -0,0 +1,16 @@
+package com.practise.microservice.feign;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.practise.microservice.entity.CurrencyConversion;
+
+
+@FeignClient(name = "currency-exchange", url = "localhost:8000")
+public interface CurrencyExchangeFiegnClient {
+
+	@GetMapping("/currency-exchange/from/{from}/to/{to}")
+	public CurrencyConversion retrieveCurrencyData(@PathVariable String from, @PathVariable String to) ;
+}
