90ed83b42a2aadf2b8bf02258e37be10704cf9f7
Bhaskar Mudaliyar <mudaliyarbhaskar29@gmail.com>
2026-03-25 17:01:52 +0530

    REtry with multiple properties with fallback reponse

--- Bhaskar_code/Microservice/currency-exchange/src/main/java/com/practise/microservice/currencyexchange/controller/CircuitBreakerControllers.java
+++ Bhaskar_code/Microservice/currency-exchange/src/main/java/com/practise/microservice/currencyexchange/controller/CircuitBreakerControllers.java
@@ -1,13 +1,27 @@
 package com.practise.microservice.currencyexchange.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import io.github.resilience4j.retry.annotation.Retry;
 
 @RestController
 public class CircuitBreakerControllers {
 
+	@Autowired
+	private RestTemplate restTemplate;
+
 	@GetMapping("/sample-api")
+	@Retry(name = "sample-api",fallbackMethod = "fallbackMethod")
 	public String getApi() {
-		return "sample API";
+		ResponseEntity<String> forEntity = restTemplate.getForEntity("http://dummy-api", String.class);
+		return forEntity.getBody();
+	}
+
+	public String fallbackMethod(Exception ex) {
+		return "Fallback";
 	}
 }
