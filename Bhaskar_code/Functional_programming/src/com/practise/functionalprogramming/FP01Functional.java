9904a9dd2f999a34ba22c3ca20eef2006fa069c2
Bhaskar Mudaliyar <mudaliyarbhaskar29@gmail.com>
2026-03-26 09:46:16 +0530

    Fetching even number by structured and Functional approach

--- Bhaskar_code/Functional_programming/src/com/practise/functionalprogramming/FP01Functional.java
+++ Bhaskar_code/Functional_programming/src/com/practise/functionalprogramming/FP01Functional.java
@@ -6,8 +6,15 @@ public class FP01Functional {
 
 	public static void main(String[] args) {
 
-		printAllNumberFromListFunctional(List.of(24, 23, 12, 4, 25, 19, 4, 5, 7, 87, 18, 12));
+		List<Integer> numbers = List.of(24, 23, 12, 4, 25, 19, 4, 5, 7, 87, 18, 12);
+	//	printAllNumberFromListFunctional(numbers);
+		printAllEvenNumberFromListFunctional(numbers);
 
+	}
+
+	private static void printAllEvenNumberFromListFunctional(List<Integer> numbers) {
+
+		numbers.stream().filter(number -> number % 2 == 0).forEach(System.out::println);
 	}
 
 	private static void printAllNumberFromListFunctional(List<Integer> numbers) {
--- Bhaskar_code/Functional_programming/src/com/practise/functionalprogramming/FP01Structured.java
+++ Bhaskar_code/Functional_programming/src/com/practise/functionalprogramming/FP01Structured.java
@@ -6,7 +6,19 @@ public class FP01Structured {
 
 	public static void main(String[] args) {
 
-		printAllNumberFromListStructured(List.of(24, 23, 12, 4, 25, 19, 4, 5, 7, 87, 18, 12));
+		List<Integer> numbers = List.of(24, 23, 12, 4, 25, 19, 4, 5, 7, 87, 18, 12);
+		//printAllNumberFromListStructured(numbers);
+		printAlEvenNumberFromListStructured(numbers);
+
+	}
+
+	private static void printAlEvenNumberFromListStructured(List<Integer> numbers) {
+		for (Integer integer : numbers) {
+			if (integer % 2 == 0) {
+				System.out.println(integer);
+			}
+
+		}
 
 	}
 
