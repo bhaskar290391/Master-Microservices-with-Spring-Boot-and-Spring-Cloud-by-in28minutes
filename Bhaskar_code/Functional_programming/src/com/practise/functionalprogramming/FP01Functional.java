0a0406e7fa76693dff3faca41a549c530a0c769f
Bhaskar Mudaliyar <mudaliyarbhaskar29@gmail.com>
2026-03-26 10:33:00 +0530

    Functional programming map function

--- Bhaskar_code/Functional_programming/src/com/practise/functionalprogramming/FP01Functional.java
+++ Bhaskar_code/Functional_programming/src/com/practise/functionalprogramming/FP01Functional.java
@@ -12,15 +12,33 @@ public class FP01Functional {
 
 		// printAllNumberFromListFunctional(numbers);
 		// printAllEvenNumberFromListFunctional(numbers);
+		
+		//printSquareEvenNumberFromListFunctional(numbers);
+		
+		//printSquareOddNumberFromListFunctional(numbers);
 
 		// numbers.stream().filter(number -> number % 2 !=
 		// 0).forEach(System.out::println);
 
 		// courses.stream().forEach(System.out::println);
-		courses.stream().filter(course -> course.contains("Spring")).forEach(System.out::println);
+		
+		//courses.stream().filter(course -> course.contains("Spring")).forEach(System.out::println);
 
-		courses.stream().filter(course -> course.length() > 4).forEach(System.out::println);
+		//courses.stream().filter(course -> course.length() > 4).forEach(System.out::println);
+		
+		courses.stream().map(course -> course+" "+course.length()).forEach(System.out::println);
 
+	}
+
+	private static void printSquareOddNumberFromListFunctional(List<Integer> numbers) {
+		numbers.stream().filter(number -> number % 2 !=0).map(number -> number * number).forEach(System.out::println);
+		
+		
+	}
+
+	private static void printSquareEvenNumberFromListFunctional(List<Integer> numbers) {
+		numbers.stream().filter(number -> number % 2==0).map(number -> number * number).forEach(System.out::println);
+		
 	}
 
 	private static void printAllEvenNumberFromListFunctional(List<Integer> numbers) {
